38a82adf0241255ad1be353f53d9f902ab9b84c6
isamrish <askmaurya48@gmail.com>
2019-11-10 23:23:32 +0530

    initial commit
    Signed-off-by: isamrish <askmaurya48@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// @flow
+
+import type { OptionsType } from "typography-options-type"
+
+const theme: OptionsType = {
+  title: "uxworks",
+  baseFontSize: "18px",
+  baseLineHeight: 1.5,
+  googleFonts: [
+    {
+      name: "Varela Round",
+      styles: ["400"]
+    }
+  ],
+  headerFontFamily: ["Varela Round", "sans-serif"],
+  bodyFontFamily: ["Varela Round", "sans-serif"],
+};
+
+export default theme;
